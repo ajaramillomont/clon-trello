dc4cd8b4be0551f1bc260e21436f89ce4e73ec0e
ajaramillomont <ajaramillomont@gmail.com>
2024-12-10 08:51:26 -0500

    agregando botones e íconos en el navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import {OverlayModule} from '@angular/cdk/overlay'
+import { OverlayModule } from '@angular/cdk/overlay'
 import { ButtonComponent } from '../../shared/components/button/button.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBell, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
-  imports: [OverlayModule, ButtonComponent],
+  imports: [OverlayModule, ButtonComponent, FontAwesomeModule],
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
+  faBell = faBell;
+  faInfoCircle = faInfoCircle;
   isOpen = false;
+  isOpenBody = false;
 }
